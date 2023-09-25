9407fcfcaa43e7eec3dc3f0e69612b6ae7207d81
noahtrejo <nmtrejo@iu.edu>
2023-09-25 14:34:13 -0400

    Facebook Login Link Redirect
    Added a login button from Facebook to login to the webpage. Currently attempting to add the permissions needed for comment filtering.

--- src/js/script1.js
+++ src/js/script1.js
@@ -1,11 +1,58 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '1012429356552180',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v17.0'
-    });
-    FB.api({
-        
-    })
-  };
+function changeColor(newColor) {
+  const elem = document.getElementById("status");
+  elem.style.color = newColor;
+}
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+  console.log('statusChangeCallback');
+  console.log(response);                   // The current login status of the person.
+  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+    testAPI();  
+  } else {                                 // Not logged into your webpage or we are unable to tell.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this webpage.';
+  }
+}
+
+
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function(response) {   // See the onlogin handler
+    statusChangeCallback(response);
+  });
+}
+
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '324016296801665',
+    cookie     : true,                     // Enable cookies to allow the server to access the session.
+    xfbml      : true,                     // Parse social plugins on this webpage.
+    version    : 'v18.0'           // Use this Graph API version for this call.
+  });
+
+  FB.login(function(response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      checkLoginState;
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  }, {
+    scope: 'email, user_likes, publish_actions', 
+    return_scopes: true
+  });
+
+
+  FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+    statusChangeCallback(response);        // Returns the login status.
+  });
+};
+
+function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
--- src/server.js
+++ src/server.js
@@ -12,7 +12,13 @@ var options = {
 
 var app = express();
 
-app.use(express.static('src'));
+app.use(express.static('src'))
+app.use('/css', express.static(__dirname + 'src/css'))
+app.use('/js', express.static(__dirname + 'src/js'))
+
+app.get('', (req, res) => {
+  res.sendFile(__dirname + '/html/index.html')
+})
 
 var server = https.createServer(options, app).listen(port, function(){
   console.log("Express server listening on port " + port);
