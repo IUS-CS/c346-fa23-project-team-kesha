95bb309cbcdbc667f9f9aa7300335bc2dccef04f
noahtrejo <nmtrejo@iu.edu>
2023-10-19 23:42:23 -0400

    This is a useless branch, I tried the sdk and I think we'll just go with obtaining data from the token requests

--- src/js/script1.js
+++ src/js/script1.js
@@ -46,11 +46,34 @@ function testAPI() {                      // Testing Graph API after login.  See
   });
 }
 
-function getCurrentUserInfo() {
+function checkLoginStateInfo() {               // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function(response) {   // See the onlogin handler
+    statusChangeCallbackInfo(response);
+  }, {
+    scope: 'email, user_likes, publish_actions', 
+    return_scopes: true
+  });
+}
+
+function statusChangeCallbackInfo(response) {  // Called with the results from FB.getLoginStatus().
+  console.log('statusChangeCallback');
+  console.log(response);                   // The current login status of the person.
+  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+    testAPIInfo();  
+  } else {                                 // Not logged into your webpage or we are unable to tell.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this webpage.';
+  }
+}
+
+function testAPIInfo() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(userInfo) {
+    console.log('Successful login for: ' + userInfo.name);
     document.getElementById('status').innerHTML = userInfo.name + ': ' + userInfo.email;
   });
-};
+}
+
 
 function logout() {
   FB.logout(function(response) {
