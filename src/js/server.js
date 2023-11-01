ff5e36be13adf100a2774110718d78f2e1887f10
Emanb <Emanbil730@gmail.com>
2023-10-31 20:04:58 -0400

    added testing.md and implemented a testing framework jest

--- src/js/server.js
+++ src/js/server.js
@@ -12,12 +12,20 @@ var options = {
 
 var app = express();
 
+module.exports = app;
+
 app.use(express.static('src'));
 
 // Meta (Facebook) App credentials
-var clientId = 'ENTER ID'; // need to replace once we get a client id
-var clientSecret = 'ENTER SECRET';
+var clientId = '1013165189995871'; // need to replace once we get a client id
+var clientSecret = 'f5c3498ab5cf5d90ec7263b3dc8a469c';
 var redirectUri = 'https://localhost:8000/auth/facebook/callback'; // need to replace once we get a url
+
+//test page
+app.get('/test', function (req, res) {
+  res.code(200);
+  res.send('This is a test page.');
+});
 
 // Endpoint to initiate OAuth
 app.get('/auth/facebook', function (req, res) {
@@ -59,7 +67,7 @@ app.get('/auth/facebook/callback', function (req, res) {
       console.log('Token data:', tokenData);
 
       // Use the access token to make API requests on behalf of the user
-      var accessToken = tokenData.access_token;
+      accessToken = tokenData.access_token;
 
       // API request to fetch user data
       https.get('https://graph.facebook.com/v18.0/me?fields=id,email,birthday', {
@@ -89,8 +97,38 @@ app.get('/auth/facebook/callback', function (req, res) {
 
 // Define a route for /profile
 app.get('/profile', function (req, res) {
-  res.send('This is the user profile page.');
+
+  // Make a request to the Graph API to fetch the user's posts
+  https.get(`https://graph.facebook.com/v18.0/me/posts?access_token=${accessToken}`, function (apiResponse) {
+    var apiData = '';
+
+    apiResponse.on('data', function (apiChunk) {
+      apiData += apiChunk;
+    });
+
+    apiResponse.on('end', function () {
+      const userPosts = JSON.parse(apiData);
+
+      // Handle user posts here
+      console.log('User posts:', userPosts);
+
+      // Send the user posts as a JSON response
+      res.json(userPosts);
+    });
+  });
 });
+
+// Define a route to automatically redirect from /profile to /profile-page
+app.get('/profile', function (req, res) {
+  res.redirect('/profile-page');
+});
+
+app.use(express.static('public')); // Serve static files from the "public" directory
+
+app.get('/profile-page', function (req, res) {
+  res.sendFile(__dirname + '/../html/profile.html');
+});
+
 
 var server = https.createServer(options, app).listen(port, function () {
   console.log("Express server listening on port " + port);
