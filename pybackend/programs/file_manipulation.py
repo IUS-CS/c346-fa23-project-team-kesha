4a79e2e6d97013cf99ba6969e1d06b5946c6b974
noahtrejo <nmtrejo@iu.edu>
2023-11-16 13:41:23 -0500

    New functions to create, delete playlists, download will now download to specific playlists

---
+++ pybackend/programs/file_manipulation.py
@@ -0,0 +1,75 @@
+from pytube import YouTube
+import shutil
+import os
+import subprocess
+
+audio_folder = "pybackend/MP3-Files"
+
+def create_new_playlist(playlist_name):
+    if not os.path.exists(audio_folder + "/" + f"{playlist_name}"):
+        try:
+            os.makedirs(audio_folder + "/" + f"{playlist_name}")
+            print("Playlist Created")
+        except:
+            print("Playlist could not be created")
+    else:
+        print("Playlist already exists")
+
+
+def remove_playlist(playlist_name):
+    if os.path.exists(audio_folder + "/" + f"{playlist_name}"):
+        try:
+            shutil.rmtree(audio_folder + "/" + f"{playlist_name}")
+            print("Playlist Deleted")
+        except:
+            print("Playlist could not be deleted")
+    else:
+        print("Playlist does not exist")
+
+
+
+def Download(link, name, playlist):
+    mp3_name = name + '.mp3'
+    mp3_file_path = audio_folder + "/" + f"{playlist}" + mp3_name
+    if os.path.exists(mp3_file_path):
+        print("File already exists")
+        return mp3_name
+    else:
+        youtubeObject = YouTube(link)
+        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        try:
+            source = youtubeObject.download()
+            head, tail = os.path.split(f"{source}")
+            subprocess.run(['ffmpeg -i "' + f"{tail}" +'" ' + mp3_name], shell=True)
+            dest = shutil.move(mp3_name, audio_folder + "/" + f"{playlist}")
+            os.remove(source)
+            print("Download Complete")
+            return mp3_name
+        except:
+            print("An error has occurred")
+
+
+# List the songs
+def show_songs(playlist):
+    dir_list = os.listdir(audio_folder + playlist + "/")
+
+    x = 0
+
+    for split in dir_list:
+        split_list = split.split(".")
+        dir_list[x] = split_list[0]
+        x += 1
+
+    print(dir_list)
+
+def show_playlists():
+    dir_list = os.listdir(audio_folder + "/")
+
+    x = 0
+
+    for split in dir_list:
+        split_list = split.split(".")
+        dir_list[x] = split_list[0]
+        x += 1
+
+    print(dir_list)
--- pybackend/programs/gui.py
+++ pybackend/programs/gui.py
@@ -1,41 +1,45 @@
 # importing required libraries
+import os
 import tkinter as tk
 from tkinter import *
 import pygame
-from videoDownload import Download
+from file_manipulation import Download, create_new_playlist, remove_playlist, show_songs, show_playlists
 
-
-
+audio_folder = "pybackend/MP3-Files"
 root = Tk()
 root.title('Team Kesha Youtube Audio Player')
-
 root.geometry("800x600")
 
-pygame.mixer.init()  # initialise the pygame
+pygame.mixer.init()  # initialize the pygame
 
-
-
-def play(song_path):
-    pygame.mixer.music.load(song_path)
+# Play Function
+def play():
+    song_name = txt1.get(1.0, "end-1c")
+    playlist = txt2.get(1.0, "end-1c")
+    pygame.mixer.music.load(audio_folder + "/" + f"{playlist}" + "/" + f"{song_name}" + ".mp3")
     pygame.mixer.music.play(loops=0)
 
+# Pause Function
 def pause():
     pygame.mixer.music.pause()
 
+# Unpause Function
 def unpause():
     pygame.mixer.music.unpause()
 
-
-# Text input
-
-
+# Download Function - downloads into audio_folder initially
 def download():
     link = txt0.get(1.0, "end-1c")
     name = txt1.get(1.0, "end-1c")
-    audio_folder = "pybackend/MP3-Files/"
-    song_name = Download(link, name)
-    song_path = audio_folder + song_name
-    play(song_path)
+    playlist = txt2.get(1.0, "end-1c")
+    song_name = Download(link, f"{name}", playlist)
+
+# Give the Current Playlist
+def show_current_playlist():
+    folder_path_list = audio_folder.split("/")
+    playlist = folder_path_list[len(folder_path_list) - 2]
+    playlist_label = Label(root, text="Playlist:\n" + playlist, font=("Courier New", 18))
+    playlist_label.pack(pady=10)
 
 
 
@@ -47,32 +51,71 @@ title.pack(side=TOP, fill=X)
 
 
 # TextBox Creation
-l = Label(root, text = "Insert Youtube Link", font=("Courier New", 14))
-txt0 = tk.Text(root, height = 2, width = 80)
-l.pack()
+link_label = Label(root, text = "Insert Youtube Link", font=("Courier New", 14))
+txt0 = tk.Text(root, height = 1, width = 40)
+link_label.pack()
 txt0.pack()
-L = Label(root, text = "Insert Song Name (No Spaces)",  font=("Courier New", 14))
-txt1 = tk.Text(root, height = 2, width = 20)
+
+L = Label(root, text = "Insert Song Name(No spaces)",  font=("Courier New", 14))
+txt1 = tk.Text(root, height = 1, width = 20)
 L.pack()
 txt1.pack()
 
-# End text input
 
-# Download and Play Button
-play_button = Button(root, text="Play Song", font=("Courier New", 32), command=download)
-play_button.pack(pady=10)
-# End Download and Play
+playlist_label = Label(root, text = "Playlist to Add",  font=("Courier New", 14))
+txt2 = tk.Text(root, height = 1, width = 20)
+playlist_label.pack()
+txt2.pack()
+# End textbox creation
 
-# Pause Button
-# Need to add a statement to only show pause when music is playing
+def show_download_button():
+    # Download Button
+
+    download_button.pack(pady=10)
+    # End Download
+def show_play_button():
+    # Play Button
+
+    play_button.pack(pady=10)
+    # End Play
+
+def show_pause_button():
+    # Pause Button
+    # Need to add a statement to only show pause when music is playing
+
+    pause_button.pack(pady=10)
+    # End Pause Button
+
+def show_unpause_button():
+
+    # Unpause Button
+    # Need to add a statement to only show pause when music is paused
+    unpause_button.pack(pady=10)
+    # End Unpause Button
+
+
+
+def show_music_buttons():
+    show_pause_button()
+    show_unpause_button()
+def remove_music_buttons():
+    pause_button.pack_forget()
+    unpause_button.pack_forget()
+
+
+
+
+# Switch Playlist button DO THIS WILL LIST PLAYLIST AS WELL AND SONGS IN PLAYLIST
+# FUNTION TO MAKE NEW PLAYLIST IS DOES NOT ALREADY EXIST
+# FUNCTION TO ADD SONG TO PLAYLIST FROM MP3 FILES IF SONG EXISTS THERE
+download_button = Button(root, text="Download Song", font=("Courier New", 32), command=download)
+play_button = Button(root, text="Play Song", font=("Courier New", 32), command=play)
 pause_button = Button(root, text="Pause", font=("Courier New", 32), command=pause)
-pause_button.pack(pady=10)
-# End Pause Button
-
-# Unpause Button
-# Need to add a statement to only show pause when music is paused
 unpause_button = Button(root, text="Unpause", font=("Courier New", 32), command=unpause)
-unpause_button.pack(pady=10)
-# End Unpause Button
+
+show_download_button()
+show_play_button()
+
+
 
 root.mainloop()
